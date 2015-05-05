2b181fbb47ab704a3622cb62ab6c40590966e428
Václav Uher <vuher@kerio.com>
2015-05-05 15:46:41 +0200

    merged with BTchat app

--- BTLockApp/app/src/main/java/com/example/vuher/btlockapp/BTLockApp.java
+++ BTLockApp/app/src/main/java/com/example/vuher/btlockapp/BTLockApp.java
@@ -1,5 +1,6 @@
 package com.example.vuher.btlockapp;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
@@ -11,14 +12,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.Message;
+import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -29,22 +35,99 @@ import java.util.UUID;
 
 
 public class BTLockApp extends ActionBarActivity {
+
+    Activity mParent;
     private BluetoothAdapter mBluetoothAdapter;
     private final int REQUEST_ENABLE_BT = 6;
+
+    private BluetoothChatService mChatService = null;
+
+    // String buffer for outgoing messages
+    private StringBuffer mOutStringBuffer;
+
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_btlock_app);
+
         setUpBluetooth();
-        registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED));
+      //  registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED));  // this is not working for connected devices
+
 
         Button boton = (Button) findViewById(R.id.button);
         boton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                listDevices();
+            public void onClick(View v) { // listener na click
+                sendMessages("unlock");
+                //  listDevices();
             }
         });
     }
+
+    private void connectDevice(BluetoothDevice device, boolean secure) {
+        // Attempt to connect to the device
+        mChatService.connect(device, secure);
+    }
+
+    /**
+     * broadcasts a message.
+     * @param message  A string of text to send.
+     */
+    private void sendMessages(String message) {
+        Log.i("info", "broadcasting unlock command");
+
+        ArrayList<String> pairedDevicesToList = new ArrayList<>();
+        String output = "";
+        try{
+            TextView tv = (TextView)findViewById(R.id.ListOfDevices);
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+// If there are paired devices
+            if (pairedDevices.size() > 0) {
+                // Loop through paired devices
+                for (BluetoothDevice device : pairedDevices) {
+                    // Add the name and address to an array adapter to show in a ListView
+                    pairedDevicesToList.add(device.getName() + "   "+ device.getAddress());
+                }
+            }
+            if(pairedDevicesToList.size() != 0){
+                for(BluetoothDevice device : pairedDevices) {
+                    //output += device.getName() + "   "+ device.getAddress()+  "   "+ device.getBondState()+" "+checkDeviceAvailability(device)+"\n";
+                    //tv.setText(output);
+                    sendMessageToDevice(message, device, true);
+                    Thread.sleep(3000);
+                }
+            }else {
+                tv.setText("This Will be a list of devices");
+            }
+        }catch(Exception Ex){
+            Log.w("chyba", Ex);
+        }
+
+        // iterate all paired devices and try to send a message
+
+    }
+
+
+    private void sendMessageToDevice(String message, BluetoothDevice device, boolean secure) {
+
+        connectDevice(device, secure); // sending is done in Handler so we do not need to sleep untill  the connection is created
+    }
+
+    private void sendMessage(String message){
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            byte[] send = message.getBytes();
+            mChatService.write(send);
+        }
+
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -76,6 +159,8 @@ public class BTLockApp extends ActionBarActivity {
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }else {
+            mChatService = new BluetoothChatService(getWindow().getContext(), mHandler);
         }
 
     }
@@ -108,48 +193,91 @@ public class BTLockApp extends ActionBarActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        System.out.println("code: " +requestCode);
+        System.out.println("code: " + requestCode);
         if(requestCode == REQUEST_ENABLE_BT) {
             System.out.println("result: " + resultCode);
             if(resultCode == Activity.RESULT_OK) {
                 showOkDialog();
-                listDevices();
+                //listDevices();
             } else if(resultCode == Activity.RESULT_CANCELED) {
                 showFailDialog();
                // listDevices();
+                // TODO exit app
             }
         }
     }
 
-    private void listDevices() {
-        Log.i("info","listing devices");
-        ArrayList<String> pairedDevicesToList = new ArrayList<>();
-        String output = "";
-        try{
-            TextView tv = (TextView)findViewById(R.id.ListOfDevices);
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-// If there are paired devices
-            if (pairedDevices.size() > 0) {
-                // Loop through paired devices
-                for (BluetoothDevice device : pairedDevices) {
-                    // Add the name and address to an array adapter to show in a ListView
-                    pairedDevicesToList.add(device.getName() + "   "+ device.getAddress());
-                }
-            }
-            if(pairedDevicesToList.size() != 0){
-                for(BluetoothDevice device : pairedDevices) {
-                    output += device.getName() + "   "+ device.getAddress()+  "   "+ device.getBondState()+" "+checkDeviceAvailability(device)+"\n";
-                    tv.setText(output);
 
-                }
-            }else {
-                tv.setText("This Will be a list of devices");
-            }
-        }catch(Exception Ex){
-            Log.w("chyba", Ex);
-        }
 
+    @Override
+    public synchronized void onPause() {
+        super.onPause();
+        if(BluetoothChatService.D) Log.e(BluetoothChatService.TAG, "- ON PAUSE -");
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(BluetoothChatService.D) Log.e(BluetoothChatService.TAG, "-- ON STOP --");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Stop the Bluetooth chat services
+        if (mChatService != null) mChatService.stop();
+        if(BluetoothChatService.D) Log.e(BluetoothChatService.TAG, "--- ON DESTROY ---");
+    }
+
+    // The Handler that gets information back from the BluetoothChatService
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case BluetoothChatService.MESSAGE_STATE_CHANGE:
+                    if(BluetoothChatService.D) Log.i(BluetoothChatService.TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            Log.d(BluetoothChatService.TAG, "CONNECTED");
+                            String message = "unlock";
+                            byte[] send = message.getBytes();
+                            mChatService.write(send);
+                            break;
+                        case BluetoothChatService.STATE_CONNECTING:
+                            Log.d(BluetoothChatService.TAG,"CONNECTING");
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                            Log.d(BluetoothChatService.TAG,"LISTENING");
+                            break;
+                        case BluetoothChatService.STATE_NONE:
+                            Log.d(BluetoothChatService.TAG,"NONE");
+                            break;
+                    }
+                    break;
+                case BluetoothChatService.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    Log.d(BluetoothChatService.TAG, writeBuf.toString());
+                    break;
+                case BluetoothChatService.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    Log.d(BluetoothChatService.TAG, readMessage);
+                    break;
+                case BluetoothChatService.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(BluetoothChatService.DEVICE_NAME);
+                    Toast.makeText(getApplicationContext(), "Connected to "
+                            + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    break;
+                case BluetoothChatService.MESSAGE_TOAST:
+                    Toast.makeText(getApplicationContext(), msg.getData().getString(BluetoothChatService.TOAST),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
 
 
     private float checkDeviceAvailability(BluetoothDevice mBluetoothDevice) {
